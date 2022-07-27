20c798d55022ec0f3877f1c0594b9d4ac0a34b68
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-27 10:04:29 -0600

    test: add test for Museum can recommend exhibits for each patron

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -1,5 +1,6 @@
 require './lib/museum.rb'
 require './lib/exhibit.rb'
+require './lib/patron.rb'
 
 describe Museum do
 
@@ -8,6 +9,11 @@ describe Museum do
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_2.add_interest("IMAX")
   end
 
     it "Museum exists" do
@@ -27,5 +33,10 @@ describe Museum do
       @dmns.add_exhibit(@dead_sea_scrolls)
       @dmns.add_exhibit(@imax)
       expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
+    end
+
+    it "Museum can recommend exhibits for patrons" do
+      expect(@dmns.recommend_exhibits(@patron_1)).to eq(@gems_and_minerals, @dead_sea_scrolls)
+      expect(@dmns.recommend_exhibits(@patron_2)).to eq(@imax)
     end
 end
