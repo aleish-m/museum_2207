c956656ae132f49d8b858917015326bcf7a1124c
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-27 09:32:52 -0600

    test: add tests to initilize patron class with name and spending money attributes

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -2,5 +2,19 @@ require './lib/patron.rb'
 
 describe Patron do
 
+  before :each do
+    @patron_1 = Patron.new("Bob", 20)
+  end
 
+    it "Patron exists" do
+      expect(@patron_1).to be_an_instance_of(Patron)
+    end
+
+    it "Patron has a name" do
+      expect(@patron_1.name).to eq("Bob")
+    end
+
+    it "Patron has a spending money" do
+      expect(@patron_1.spending_money).to eq(20)
+    end
 end
