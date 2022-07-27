ca2f8ce9107162402e5be126a6e4a4e0edcdf418
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-27 10:26:46 -0600

    feat: add recomend exhibites method

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -6,9 +6,11 @@ describe Museum do
 
   before :each do
     @dmns = Museum.new("Denver Museum of Nature and Science")
+
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
+
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
     @patron_1.add_interest("Dead Sea Scrolls")
@@ -35,8 +37,11 @@ describe Museum do
       expect(@dmns.exhibits).to eq([@gems_and_minerals, @dead_sea_scrolls, @imax])
     end
 
-    it "Museum can recommend exhibits for patrons" do
-      expect(@dmns.recommend_exhibits(@patron_1)).to eq(@gems_and_minerals, @dead_sea_scrolls)
-      expect(@dmns.recommend_exhibits(@patron_2)).to eq(@imax)
+    it "Museum can recommend exhibits for each patron" do
+      @dmns.add_exhibit(@gems_and_minerals)
+      @dmns.add_exhibit(@dead_sea_scrolls)
+      @dmns.add_exhibit(@imax)
+      expect(@dmns.recommend_exhibits(@patron_1)).to eq([@gems_and_minerals, @dead_sea_scrolls])
+      expect(@dmns.recommend_exhibits(@patron_2)).to eq([@imax])
     end
 end
